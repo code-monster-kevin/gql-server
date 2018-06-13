ec9c0835b953ce623db4d5f27a4a56a7ba6f0e2c
Kevin Wu <kevin.wu@live.com.my>
2018-06-13 17:55:18 +0800

    base Apollo GraphQL server

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = [`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+  
+  schema {
+    query: Query
+  }
+`]
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+var schema = makeExecutableSchema({typeDefs, resolvers});
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+app.listen(4001, () => console.log('Now browse to localhost:4001/graphiql'));
